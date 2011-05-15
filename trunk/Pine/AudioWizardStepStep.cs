6427a179e79bfd8f729160eaa237a25e5f28e761
soap4me@gmail.com <soap4me@gmail.com@41c5003c-aa96-8be3-ed4b-3e92e22b352b>
2011-05-15 18:57:22 +0000

    Тесты для шагов мастера первоначальной настройки

---
+++ trunk/Pine/AudioWizardStepStep.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Pine
+{
+    public class AudioWizardStepStep
+    {
+        private readonly IAudioConfiguration _audioConfiguration;
+        private readonly IRadioFactory _radioFactory;
+        private IRadio _radio;
+
+        public AudioWizardStepStep(IAudioConfiguration audioConfiguration, IRadioFactory radioFactory)
+        {
+            _audioConfiguration = audioConfiguration;
+            _radioFactory = radioFactory;
+        }
+
+        public string InputId
+        {
+            get { throw new NotImplementedException(); }
+            set { _audioConfiguration.InputId = value; }
+        }
+
+        public string OutputId
+        {
+            get { throw new NotImplementedException(); }
+            set { _audioConfiguration.OutputId = value; }
+        }
+
+        public bool CheckParameters
+        {
+            get { throw new NotImplementedException(); }
+            set
+            {
+                if (value)
+                {
+                    _radio = _radioFactory.CreateRadio(_audioConfiguration);
+                }
+                else
+                {
+                    _radio.Dispose();
+                }
+
+            }
+        }
+
+        public double Frequency
+        {
+            get { throw new NotImplementedException(); }
+            set { _radio.Frequency = value; }
+        }
+    }
+}
---
+++ trunk/Pine/IRadioFactory.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Pine
+{
+    public interface IRadioFactory
+    {
+        IRadio CreateRadio(IAudioConfiguration audioConfiguration);
+    }
+}
